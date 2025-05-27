2e62fb2e78d4120b8ca2bdf1228b25eb8d8a2934
Marat <marat.saifulin@gmail.com>
2025-05-27 19:54:12 +0300

    Добавил файлы в test.py

---
+++ main/test.py
@@ -0,0 +1,13 @@
+class House:
+    def __init__(self)
+        self.name = name
+        self.price = price
+        self.size = size
+        self.color = color
+
+
+if __name__ == '__main__':
+
+
+
+
